77b4530ac493763f0f5ac47d3d6b26771aa007f4
Rakesh-CS <rakeshcs.2000@gmail.com>
2023-05-15 21:30:47 +0530

    sprint3

---
+++ JAS_Intial_Learnings/Sprint3/Chapter2.py
@@ -0,0 +1,12 @@
+# import rasa_nlu
+
+# from rasa_nlu.training_data  import load_data
+# from rasa_nlu.config import RasaNLUModelConfig
+# from rasa_nlu.model import Trainer
+# from rasa_nlu import config
+
+# train_data = load_data('rasa_dataset.json')
+# trainer = Trainer(config.load("config_spacy.yaml"))
+
+import sklearn
+print(sklearn.__version__)
